5b99a51a948df8b0fb7fc57907ef25afe8f33d86
Aman Shah (Software Developer) <aman.shah@enableit.dk>
2020-12-12 20:30:44 +0530

    Updated main function

--- src/ecommerce/core.clj
+++ src/ecommerce/core.clj
@@ -6,11 +6,11 @@
 (defn -main
   "Main Function that gets called and generate the relevant csv and json files"
   [& args]
-  (let [mods ((read-json-file "lipstick.json") :mods)
-        listItems (mods :listItems)]
+  (let [json-content (read-json-file "lipstick.json")
+        listItems (get-in json-content [:mods :listItems])]
     (do
       (write-to-csv "first.csv" (prUrl-pr-opr-skus listItems))
       (write-to-csv "second.csv" (oem-sku listItems))
       (spit "third.json" (group-brandName listItems))
-      (write-to-csv "fourth.csv" (find-image mods))
+      (write-to-csv "fourth.csv" (find-image json-content))
       (write-to-csv "fifth.csv" (buy-products listItems 250)))))
