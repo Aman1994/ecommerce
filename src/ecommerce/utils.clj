a31903d9279a7e95d66da1bbf44f3201e185b034
Aman Shah (Software Developer) <aman.shah@enableit.dk>
2020-12-12 20:29:35 +0530

    added some more utilities functions

--- src/ecommerce/utils.clj
+++ src/ecommerce/utils.clj
@@ -13,3 +13,23 @@
   [filename content]
   (with-open [writer (io/writer filename)]
     (csv/write-csv writer content)))
+
+(defn make-map
+  "Returns a clojure map, if it finds vectors then maps them with their indices"
+  [mp]
+  (reduce
+    (fn [l [k v]]
+      (assoc l k (cond
+                   (map? v) (make-map v)
+                   (coll? v) (make-map (zipmap (range) v))
+                   :else v)))
+    {} mp))
+
+(defn flatten-json
+  "Flattens the json"
+  [prefix separator mp]
+  (reduce-kv
+    #(let [fld-key (if prefix (str prefix separator %2) %2)]
+       (if (map? %3)
+         (merge %1 (flatten-json fld-key separator %3))
+         (assoc %1 fld-key %3))) {} mp))
